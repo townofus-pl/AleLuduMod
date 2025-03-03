1b2290f91e79c143166e59d55ab33b19f7a9de1f
bazillos <46715473+bazillos@users.noreply.github.com>
2025-03-03 21:06:11 +0100

    Feature/config (#5)
    * dynamic layout config
    
    * config panel

--- src/AleLuduMod/AleLuduModPlugin.cs
+++ src/AleLuduMod/AleLuduModPlugin.cs
@@ -3,9 +3,11 @@ using BepInEx;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 using Reactor;
+using BepInEx.Configuration;
 using Reactor.Networking;
 using Reactor.Networking.Attributes;
 using System.Linq;
+using BepInEx.Logging;
 
 namespace AleLuduMod;
 
@@ -13,18 +15,21 @@ namespace AleLuduMod;
 [BepInProcess("Among Us.exe")]
 [BepInDependency(ReactorPlugin.Id)]
 [BepInDependency("gg.reactor.debugger", BepInDependency.DependencyFlags.SoftDependency)] // fix debugger overwriting MinPlayers
+[BepInDependency("com.slushiegoose.townofus", BepInDependency.DependencyFlags.SoftDependency)] // load after town of us
 public partial class AleLuduModPlugin : BasePlugin
 {
     public const int MaxPlayers = 35;
     public const int MaxImpostors = 35 / 2;
-
+    public static ConfigEntry<bool> Force4Columns { get; set; }
     private Harmony Harmony { get; } = new(Id);
 
     public override void Load()
-    {
+    { 
         NormalGameOptionsV08.RecommendedImpostors = NormalGameOptionsV08.MaxImpostors = Enumerable.Repeat(35, 35).ToArray();
         NormalGameOptionsV08.MinPlayers = Enumerable.Repeat(4, 35).ToArray();
         HideNSeekGameOptionsV08.MinPlayers = Enumerable.Repeat(4, 35).ToArray();
+
+        Force4Columns = Config.Bind("Settings", "Force 4 columns", true, "Always display 4 columns in meeting, vitals, etc.");
 
         Harmony.PatchAll();
     }
