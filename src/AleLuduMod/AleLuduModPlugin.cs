41f8d14a031579d24850448e5b1dd26584f0211a
gitq <90698953+raspberrygitq@users.noreply.github.com>
2025-09-25 18:33:10 +0200

    Support for New Create Game Menu
    - CreateGameOptions has been removed because it is unnecessary.
    - PingTracker has been removed, but replaced with the ReactorCredits.
    
    Tested on versions 16.0.0 - 17.0.0 and everything works correctly.

--- src/AleLuduMod/AleLuduModPlugin.cs
+++ src/AleLuduMod/AleLuduModPlugin.cs
@@ -1,30 +1,26 @@
-﻿using AmongUs.GameOptions;
-using BepInEx;
+﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 using Reactor;
-using BepInEx.Configuration;
-using System.Linq;
+using Reactor.Utilities;
 
 namespace AleLuduMod;
 
-[BepInAutoPlugin("pl.townofus.aleludu")]
+[BepInAutoPlugin("pl.aleludumod.plugin")]
 [BepInProcess("Among Us.exe")]
 [BepInDependency(ReactorPlugin.Id)]
 [BepInDependency("gg.reactor.debugger", BepInDependency.DependencyFlags.SoftDependency)] // fix debugger overwriting MinPlayers
-[BepInDependency("com.slushiegoose.townofus", BepInDependency.DependencyFlags.SoftDependency)] // load after town of us
 public partial class AleLuduModPlugin : BasePlugin
 {
-    public const int MaxPlayers = 36;
-    public const int MaxImpostors = 36 / 2;
+    public const int MaxPlayers = 35;
+    public const int MaxImpostors = MaxPlayers / 2;
     public static ConfigEntry<bool> Force4Columns { get; set; }
     private Harmony Harmony { get; } = new(Id);
 
     public override void Load()
-    { 
-        NormalGameOptionsV09.RecommendedImpostors = NormalGameOptionsV09.MaxImpostors = Enumerable.Repeat(36, 36).ToArray();
-        NormalGameOptionsV09.MinPlayers = Enumerable.Repeat(4, 36).ToArray();
-        HideNSeekGameOptionsV09.MinPlayers = Enumerable.Repeat(4, 36).ToArray();
+    {
+        ReactorCredits.Register<AleLuduModPlugin>(ReactorCredits.AlwaysShow);
 
         Force4Columns = Config.Bind("Settings", "Force 4 columns", true, "Always display 4 columns in meeting, vitals, etc.");
 
